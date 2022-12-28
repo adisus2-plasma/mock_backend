b4215d502a9c8ef2395ddee22d291f85e40aaecc
pattarapol <pattarapol.sangsunee@mail.kmutt.ac.th>
2022-12-28 14:29:53 +0700

    [UPDATE]Feed api

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const PORT = process.env.PORT || 9005
+const PORT = process.env.PORT || 9005;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/health', (req, res) => {
-    res.send({
-        status: 'This service are healthty.'
-    })
-})
+app.get("/health", (req, res) => {
+  res.send({
+    status: "This service are healthty.",
+  });
+});
 
 app.listen(PORT, () => {
-    console.log('Server are up on port ' + PORT)
-})
+  console.log("Server are up on port " + PORT);
+});
